7f6b6065ee1f2cc626c5ac109eb381a82c56f888
Misha <aptukovm@yandex.ru>
2024-02-17 22:19:25 +0300

    fix: lab1_new_

---
+++ lab1-2/main.py
@@ -0,0 +1,7 @@
+from graphics import print_graphics
+from characters import print_characters
+
+
+if __name__ == "__main__":
+    print_graphics([20, 50, 800])
+    print_characters([20, 100, 1000])
